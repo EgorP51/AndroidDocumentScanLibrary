4c7f4eff5062502d561e11c32ca1fe1aa3cc7dc3
yehor-pustovoit <63584ee@gmail.com>
2025-01-20 15:16:28 +0200

    try to fix

--- ScanDemoExample/scanlibrary/src/main/java/com/scanlibrary/Utils.java
+++ ScanDemoExample/scanlibrary/src/main/java/com/scanlibrary/Utils.java
@@ -22,13 +22,30 @@ public class Utils {
 
     public static Uri getUri(Context context, Bitmap bitmap) {
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        int quality = 100;
+        final int MAX_SIZE = 2 * 1024 * 1024;
+
+        do {
+            bytes.reset();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, bytes);
+            int sizeInBytes = bytes.size();
+
+            quality -= 5;
+
+            if (quality < 0) {
+                Log.wtf("IMAGE SIZE", "Image cannot be compressed below 0% quality");
+                return null;
+            }
+
+            Log.wtf("CURRENT QUALITY", "Quality: " + quality + ", Size: " + sizeInBytes + " bytes");
+        } while (bytes.size() > MAX_SIZE);
+
         Log.wtf("PATH", "before insertImage");
-        // String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Title", null);
         String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Title" + " - " + (currentTime = Calendar.getInstance().getTime()), null);
         Log.wtf("PATH", path);
         return Uri.parse(path);
     }
+
 
     public static Bitmap getBitmap(Context context, Uri uri) throws IOException {
         return MediaStore.Images.Media.getBitmap(context.getContentResolver(), uri);
